8407815faf8a38ee84bec549e8bd1ebfd28f7e21
SpookScruce <spookspruce@gmail.com>
2024-10-13 13:43:55 +0100

    Added canvas width and height

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('biomeCanvas');
 const ctx = canvas.getContext('2d');
-
 const gridWidth = 50;
 const gridHeight = 50;
 const cellSize = 10;  
+
+canvas.width = gridWidth * cellSize;
+canvas.height = gridHeight * cellSize;
